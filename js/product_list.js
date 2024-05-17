c587e61ce0ba26c5d4e7fe9de1f0cc64a919639f
lee-hyesu <ragnarok94@naver.com>
2024-05-17 17:14:29 +0900

    혜수_제품리스트 수정+모바일 버전 작성+js추후 수정

--- js/product_list.js
+++ js/product_list.js
@@ -2,7 +2,6 @@ $(window).on('scroll', () => {
   if(document.documentElement.scrollTop >= 500) {
     $('.product-list .product-list-img li .best-seller-box').addClass('active');
     $('.product-list .product-list-img .product-list-item1 img, .product-list-item2 img, .product-list-item3 img, .product-list-item4 img').addClass('active');
-    // $('.product-list .product-list-img li .product-list-inner-wrapper').addClass('active');
 
     if(document.documentElement.scrollTop >= 1100) {
       $('.product-list .product-list-img .product-list-item5 img, .product-list-item6 img, .product-list-item7 img, .product-list-item8 img').addClass('active');
@@ -12,10 +11,14 @@ $(window).on('scroll', () => {
       $('.product-list .product-list-img .product-list-item9 img, .product-list-item10 img, .product-list-item11 img, .product-list-item12 img').addClass('active');
     }
 
-    if(document.documentElement.scrollTop >= 2200) {
+    if(document.documentElement.scrollTop >= 2300) {
       $('.product-list .product-list-img .product-list-item13 img, .product-list-item14 img').addClass('active');
     }
   } else{
     $('.nav').removeClass('active');
   }
-})
+});
+
+// $('.product-list .product-list-img li img').on('mouseover', () => {
+//   $('.product-list .product-list-img li .product-list-img-option-box').addClass('active');
+// });
