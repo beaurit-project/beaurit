ecc8c09e84690f117688c2506dc7f7312e3cfe5c
jinnyaa <oio99661@gmail.com>
2024-05-20 23:54:33 +0900

    진선_html통합

--- js/main-rotate.js
+++ js/main-rotate.js
@@ -1,39 +1,41 @@
-window.onscroll = function () {
-    scrollRotate(); 
-};
-  
+window.addEventListener('scroll', function() {
+    scrollRotate();
+});
+
 function scrollRotate() {
     let scrollTop = window.pageYOffset;
     let scrollHeight = document.body.scrollHeight;
     let windowHeight = window.innerHeight;
 
-    let brandStoryContainer = document.querySelector('.brand-story-container');
-    let brandStoryContainerRect = brandStoryContainer.getBoundingClientRect();
-    let containerBottom = brandStoryContainerRect.bottom;
+    let brandStoryTxtBox = document.querySelector('.brand-story-txt-box.stop img');
+    let brandStoryTxtBoxRect = brandStoryTxtBox.getBoundingClientRect();
+    let txtBoxCenter = brandStoryTxtBoxRect.top + brandStoryTxtBoxRect.height / 2;
+    let windowCenter = windowHeight / 2;
 
-    let rotateValue = 360 * (scrollTop / (scrollHeight - windowHeight));
-    if (containerBottom <= 0) {
-        rotateValue = 360; 
-    } else if (rotateValue >= 360) {
-        rotateValue = 0; 
+    if (Math.abs(txtBoxCenter - windowCenter) < 10) { 
+        setRotateValue(360);
+    } else {
+        let rotateValue = 360 * (scrollTop / (scrollHeight - windowHeight));
+        setRotateValue(rotateValue);
     }
-
-    let image1 = document.getElementById("rotate-img1");
-    image1.style.transform = "rotate(" + rotateValue + "deg)";
-    
-    let image2 = document.getElementById("rotate-img2");
-    image2.style.transform = "rotate(" + (-rotateValue) + "deg)";
-
-    let image3 = document.getElementById("rotate-img3");
-    image3.style.transform = "rotate(" + rotateValue + "deg)";
-
-    let image4 = document.getElementById("rotate-img4");
-    image4.style.transform = "rotate(" + (-rotateValue) + "deg)";
-
-    let image5 = document.getElementById("rotate-img5");
-    image5.style.transform = "rotate(" + rotateValue + "deg)";
-
-    let image6 = document.getElementById("rotate-img6");
-    image6.style.transform = "rotate(" + (-rotateValue) + "deg)";
 }
 
+function setRotateValue(value) {
+    let image1 = document.getElementById("rotate-img1");
+    image1.style.transform = "rotate(" + value + "deg)";
+    
+    let image2 = document.getElementById("rotate-img2");
+    image2.style.transform = "rotate(" + (-value) + "deg)";
+
+    let image3 = document.getElementById("rotate-img3");
+    image3.style.transform = "rotate(" + value + "deg)";
+
+    let image4 = document.getElementById("rotate-img4");
+    image4.style.transform = "rotate(" + (-value) + "deg)";
+
+    let image5 = document.getElementById("rotate-img5");
+    image5.style.transform = "rotate(" + value + "deg)";
+
+    let image6 = document.getElementById("rotate-img6");
+    image6.style.transform = "rotate(" + (-value) + "deg)";
+}
