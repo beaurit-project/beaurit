944462ae4c577d065848bf712d83837b0b41e50d
lee-hyesu <ragnarok94@naver.com>
2024-05-21 15:05:59 +0900

    혜수_main-sub 수정

--- js/main-sub.js
+++ js/main-sub.js
@@ -1,32 +1,3 @@
-// $(document).ready(function () {
-//   const $button = $('.main-bg .main-sub-box .button-container');
-//   const $images = $('.main-bg-contents');
-//   let intervalId;
-
-//   function activateImages() {
-//     $images.each(function (index) {
-//       const $img = $(this);
-//       setTimeout(function () {
-//         $img.addClass('active');
-//       }, index * 1500); // 이미지가 순차적으로 나타나도록 1.5초 간격으로 설정
-//     });
-
-//     // setTimeout(function() {
-//     //   $images.removeClass('active');
-//     // }, $images.length * 1500); // 모든 이미지가 활성화된 후 비활성화
-//   }
-
-//   $button.on('mouseover', function () {
-//     activateImages();
-//     intervalId = setInterval(activateImages, $images.length * 1500 + 500); // 이미지가 반복되도록 인터벌 설정
-//   });
-
-//   $button.on('mouseout', function () {
-//     clearInterval(intervalId);
-//     $images.stop(true, true).removeClass('active');
-//   });
-// });
-
 /* 이미지 구역 hover 시, background 생김 */
 $('.sub-main .sub-main-container .sub-main-box1').on('mouseenter', () => {
   $('.sub-main .sub-main-container .sub-main-box1 .sub-main-img-bg').addClass('active');
@@ -129,8 +100,8 @@ document.addEventListener('DOMContentLoaded', () => {
       const offsetX = e.clientX - rect.left;
       const offsetY = e.clientY - rect.top;
       
-      const moveX = (offsetX / rect.width - 0.5) * 40; // 이동 강도 조절
-      const moveY = (offsetY / rect.height - 0.5) * 40; // 이동 강도 조절
+      const moveX = (offsetX / rect.width - 0.5) * 30; // 이동 강도 조절
+      const moveY = (offsetY / rect.height - 0.5) * 30; // 이동 강도 조절
       
       element.style.transform = `translate(${moveX}px, ${moveY}px)`;
   });
@@ -150,8 +121,8 @@ document.addEventListener('DOMContentLoaded', () => {
       const offsetX = e.clientX - rect.left;
       const offsetY = e.clientY - rect.top;
       
-      const moveX = (offsetX / rect.width - 0.5) * 40; // 이동 강도 조절
-      const moveY = (offsetY / rect.height - 0.5) * 40; // 이동 강도 조절
+      const moveX = (offsetX / rect.width - 0.5) * 30; // 이동 강도 조절
+      const moveY = (offsetY / rect.height - 0.5) * 30; // 이동 강도 조절
       
       element.style.transform = `translate(${moveX}px, ${moveY}px)`;
   });
@@ -169,8 +140,8 @@ document.addEventListener('DOMContentLoaded', () => {
       const offsetX = e.clientX - rect.left;
       const offsetY = e.clientY - rect.top;
       
-      const moveX = (offsetX / rect.width - 0.5) * 40; // 이동 강도 조절
-      const moveY = (offsetY / rect.height - 0.5) * 40; // 이동 강도 조절
+      const moveX = (offsetX / rect.width - 0.5) * 30; // 이동 강도 조절
+      const moveY = (offsetY / rect.height - 0.5) * 30; // 이동 강도 조절
       
       element.style.transform = `translate(${moveX}px, ${moveY}px)`;
   });
